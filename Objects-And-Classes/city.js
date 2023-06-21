2d14e0aea1e15edc5cf347faf14e42017c0667ef
Kalin Georgiev <100343638+kalingg@users.noreply.github.com>
2023-06-21 21:31:40 +0300

    Add files via upload

---
+++ Objects-And-Classes/city.js
@@ -0,0 +1,11 @@
+city={
+    name: "Sofia",
+    area: 492,
+    population: 1238438,
+    country: "Bulgaria",
+    postCode: "1000" 
+}
+
+for(let key in city){
+    console.log(key +" -> " + city[key]);
+}
